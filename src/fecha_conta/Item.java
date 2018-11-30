02314f4ded3349a31e6c1f03638cb137c7c932cc
Fernando Henrique <fernandodojo@gmail.com>
2018-11-30 12:12:40 -0300

    Gets e Sets Item

--- src/fecha_conta/Item.java
+++ src/fecha_conta/Item.java
@@ -11,11 +11,43 @@ public class Item {
     double valorUni;
     double valorTot;
 
-    Item(int numItem, int qtdItem) {
+    Item(int numItem, int qtdItem, double valorUni, double valorTot) {
         this.numItem = numItem;
         this.qtdItem = qtdItem;
-        //double valorUni;
-        //double valorTot;
+        this.valorTot = valorUni;
+        this.valorTot = valorTot;
+    }
+
+    public int getNumItem() {
+        return numItem;
+    }
+
+    public void setNumItem(int numItem) {
+        this.numItem = numItem;
+    }
+
+    public int getQtdItem() {
+        return qtdItem;
+    }
+
+    public void setQtdItem(int qtdItem) {
+        this.qtdItem = qtdItem;
+    }
+
+    public double getValorUni() {
+        return valorUni;
+    }
+
+    public void setValorUni(double valorUni) {
+        this.valorUni = valorUni;
+    }
+
+    public double getValorTot() {
+        return valorTot;
+    }
+
+    public void setValorTot(double valorTot) {
+        this.valorTot = valorTot;
     }
 
 }
