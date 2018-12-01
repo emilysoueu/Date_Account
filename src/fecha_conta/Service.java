3569af5335268a492f9723710f7daaa50926891b
Unknown <emilys2costa@gmail.com>
2018-12-01 12:53:35 -0300

    add método de editar pedido
    Incompleto!

--- src/fecha_conta/Service.java
+++ src/fecha_conta/Service.java
@@ -7,32 +7,32 @@ package fecha_conta;
 import java.util.ArrayList;
 
 public class Service {
-
+    
     ArrayList<Clientes> listaCliente;
     int totalFechado; // contas em aberto
     int totalAberto; // contas fechadas
     int garcom;
     double gorjeta;
-
+    
     public Service(int totalFechado, int totalAberto, int garcom, double gorjeta) {
         this.totalAberto = totalAberto;
         this.totalFechado = totalFechado;
         this.listaCliente = new ArrayList<>();
         this.garcom = garcom;
     }
-
+    
     public int getGarcom() {
         return garcom;
     }
-
+    
     public void setGarcom(int garcom) {
         this.garcom = garcom;
     }
-
+    
     public int getTotalAberto() {
         return this.totalAberto;
     }
-
+    
     public void setTotalAberto() {
         int t = 0;
         for (Clientes aux : listaCliente) {
@@ -42,7 +42,7 @@ public class Service {
         }
         this.totalAberto = t;
     }
-
+    
     public void setTotalFechado() {
         int t = 0;
         for (Clientes aux : listaCliente) {
@@ -52,20 +52,33 @@ public class Service {
         }
         this.totalFechado = t;
     }
-
+    
     public int getTotalFechado() {
         return totalFechado;
     }
-
+    
     public double getGorjeta() {
         return gorjeta;
     }
-
+    
     public void setGorjeta() {
         for (Clientes aux : this.listaCliente) {
             this.gorjeta += aux.getGorjeta();
         }
-
+        
     }
-
+    
+    void clienteDelItem(int numCliente, int numItem) {
+        int i = 1;
+        for (Clientes aux : this.listaCliente) {
+            i++;
+            if (i == numCliente) {
+                aux.delItem(numItem);
+                System.out.println("\n Item alterado com sucesso\n");                
+            }
+            
+        }
+    }
+    
 }
+
